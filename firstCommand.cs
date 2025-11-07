e384413544e133b0a3810f3da879ab24d996a9e2
Space <andrewtheis4@gmail.com>
2025-11-07 14:23:46 -0800

    first command allows users see the current first redeem leaderboard

---
+++ firstCommand.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Streamer.bot.Plugin.Interface;
+
+
+// Must update class name to CPHInline when porting over to Streamer.bot C# sub-action
+public class CPHInline : CPHInlineBase
+{
+    public bool Execute()
+    {
+        List<KeyValuePair<string, int>> topUsers = CPH.GetGlobalVar<List<KeyValuePair<string, int>>>("global_currentTopFive");
+        if (topUsers == null)
+        {
+            CPH.SendMessage("Top 5 not found!");
+        }
+        CPH.SendMessage("🏆 MO and Marie's favorites 🏆:");
+        for (int i = 0; i < topUsers.Count; i++)
+        {
+            int rank = i + 1;
+            KeyValuePair<string, int> pair = topUsers[i];
+            string userName = pair.Key;
+            int firstCount = pair.Value;
+            CPH.SendMessage($"#{rank}) {userName} - {firstCount}");
+        }
+        return true;
+    }
+}
