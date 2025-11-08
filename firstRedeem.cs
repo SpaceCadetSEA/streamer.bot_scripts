8e14d443285a5b370138dff75178f14122474f44
Andrew <andrewtheis4@gmail.com>
2025-11-08 01:23:31 -0800

    updating documentation

--- firstRedeem.cs
+++ firstRedeem.cs
@@ -9,16 +9,20 @@ public class CPHFirstUsers : CPHInlineBase
     {
         CPH.TryGetArg("userName", out string userName);
         CPH.TryGetArg("userCounter", out int userCounter);
-        // read in the firsts dictionary object from global variable?
-        // CPH.TryGetArg("global_viewerFirstCount", out Dictionary<string, int> viewerFirstCount);
+
         Dictionary<string, int> viewerFirstCount = CPH.GetGlobalVar<Dictionary<string, int>>("global_viewerFirstCount");
-        // try to add user to counter
+        // Fall back if the global variable does not yet exist.
         viewerFirstCount ??= new Dictionary<string, int>();
+
         // Overwrite existing userCounter (first redeem) for user
         viewerFirstCount[userName] = userCounter;
         CPH.SetGlobalVar("global_viewerFirstCount", viewerFirstCount);
 
         // Generate top 5 users from first redeem
+        // By placing this here, we will only run this code once per stream.
+        // This could be an expensive operation, but even with 10,000 individual
+        // entries in the viewerFirstCount dictionary, this should be performant
+        // enough.
         List<KeyValuePair<string, int>> topUsers = [.. viewerFirstCount];
         topUsers.Sort((x, y) => x.Value);
         // limit to 5
